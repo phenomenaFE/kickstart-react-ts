6eab616519cabc3137757bb256b3b82b8728d9d6
hazem hisham <hazem.frontend@gmail.com>
2022-10-17 13:37:44 +0200

    kickstart

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/Assets/Styles/App.css';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <Home/>
       
     </div>
   );
---
+++ src/Setup/roues-manage/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+
+const router=createBrowserRouter(
+    [
+        {
+            path:'/',
+            element:<></>
+        }
+    ]
+)
+
+

539e036d66757baf76280568f04fee933c8fe999
hazem hisham <hazem.frontend@gmail.com>
2022-10-17 14:25:05 +0200

    kickstart

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import React from 'react';
+// https://www.figma.com/file/sfhbAjBDDlKrdvthrMA8B4/Bookland_BookStoreEcommerceWebsite_AllPage?node-id=8%3A4716
 function Home() {
     return ( 
         <Container fluid>
